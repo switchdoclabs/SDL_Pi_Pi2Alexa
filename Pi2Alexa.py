c23da1d8a99e06a9d2e6e1397976fb32ff0eb2ea
John Shovic <jshovic@switchdoc.com>
2018-01-25 19:33:02 -0800

    First Commit

---
+++ Pi2Alexa.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+#
+#
+# Raspberry Pi to Alexa 
+#
+# SwitchDoc Labs
+#
+
+PI2ALEXAVERSION = "002"
+
+#imports 
+
+import sys
+import os
+import RPi.GPIO as GPIO
+import time
+import json
+
+from datetime import datetime
+from datetime import timedelta
+
+from pubnub.pubnub import PubNub
+from pubnub.pubnub import PNConfiguration
+from pubnub.callbacks import SubscribeCallback
+
+
+# debug setting
+DEBUG = True
+
+# PubNub configuration
+
+Pubnub_Publish_Key = "pub-c-xxx"
+Pubnub_Subscribe_Key = "sub-c-xxx"
+
+#GPIO LED Pin configuration
+
+LED_PIN=4
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(LED_PIN,GPIO.OUT)
+GPIO.output(LED_PIN, GPIO.LOW)
+
+
+#GPIO State
+
+LED_State = "Off"
+
+pnconf = PNConfiguration()
+ 
+pnconf.subscribe_key = Pubnub_Subscribe_Key
+pnconf.publish_key = Pubnub_Publish_Key
+  
+
+pubnub = PubNub(pnconf)
+
+def publish_callback(result, status):
+        if (DEBUG):
+		print "status.is_error", status.is_error()
+		print "status.original_response", status.original_response
+		pass
+        # handle publish result, status always present, result if successful
+        # status.isError to see if error happened
+
+def publishStatusToPubNub():
+
+        myMessage = {}
+        myMessage["Pi2Alexa_CurrentStatus"] = "Active" 
+	myMessage["Pi2Alexa_Version"] = '{}'.format(PI2ALEXAVERSION) 
+        myMessage["TimeStamp"] = '{}'.format( datetime.now().strftime( "%m/%d/%Y %H:%M:%S"))
+        myMessage["Pi2Alexa_LEDState"] = LED_State 
+        
+        if (DEBUG):
+        	print myMessage
+
+        pubnub.publish().channel('Pi2Alexa_Status').message(myMessage).async(publish_callback)
+
+
+
+ 
+class AlexaMyListener(SubscribeCallback):
+    def status(self, pubnub, status):
+	pass 
+    def message(self, pubnub, message):
+
+	print "message=", message.message
+	# set LED commands
+	LED_State = message.message["LED"]
+        print "LED_State = ", LED_State
+
+	if (LED_State == "Off"):
+		GPIO.output(LED_PIN, GPIO.LOW)
+
+	if (LED_State == "On"):
+		GPIO.output(LED_PIN, GPIO.HIGH)
+
+ 
+    def presence(self, pubnub, presence):
+        pass
+
+# set up subscribe channel
+
+my_listener = AlexaMyListener()
+ 
+pubnub.add_listener(my_listener)
+ 
+pubnub.subscribe().channels("Pi2Alexa_Data").execute()
+
+
+while True:
+
+	publishStatusToPubNub()
+	time.sleep(60.0)	
+
---
+++ README.md
@@ -0,0 +1,23 @@
+#
+#  Pi2Alexa Alexa Raspberry Pi Interface
+# SDL_Pi_Pi2Alexa
+#
+# SwitchDoc Labs January 2018
+#
+
+# more Documentation on www.switchdoc.com
+# See detailed tutorial available from switchdoc labs
+
+January 24 2, 2018:  Initial Release Version 002
+
+SDL2AlexaPi.zip contains the Lambda function for Pi2Alexa  - see tutorial for Pi2Alexa
+
+# basic install instructions for supporting libraries
+
+sudo apt-get update <BR>
+sudo apt-get dist-upgrade <BR>
+
+#Installing PubNub
+
+sudo pip install 'pubnub>=4.0.5' <BR>
+
